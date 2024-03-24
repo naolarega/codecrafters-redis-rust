63fd2877be655b6bcb421e1b9a660ed36ed0b84f
Naol Arega <naol.dev@gmail.com>
2024-03-24 23:13:56 +0300

    pass 3rd stage

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream.write_all(b"+PONG\r\n").unwrap();
+                for _ in 0..2 {
+                    let mut buf = [u8::default()];
+                    stream.read(&mut buf).unwrap();
+                    stream.write_all(b"+PONG\r\n").unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
