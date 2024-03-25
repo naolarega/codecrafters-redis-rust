f71bf87189447fb1f6c34868299bece30d963df0
Naol Arega <naol.dev@gmail.com>
2024-03-25 22:30:07 +0300

    completed 4th stage

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,26 @@
 use std::{
     io::{Read, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
+    thread
 };
+
+fn handle(mut stream: TcpStream) {
+    loop {
+        let mut buf = [u8::default(); 512];
+        let n = stream.read(&mut buf).unwrap();
+
+        if n == 0 {
+            break;
+        }
+
+        stream.write(b"+PONG\r\n").unwrap();
+    }
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                loop {
-                    let mut buf = [u8::default(); 512];
-                    let n = stream.read(&mut buf).unwrap();
-
-                    if n == 0 {
-                        break;
-                    }
-
-                    stream.write(b"+PONG\r\n").unwrap();
-                }
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+    for stream in listener.incoming().flatten() {
+        thread::spawn(|| handle(stream));
     }
 }
